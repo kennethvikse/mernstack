9cadb237bdba893cd3d0c55a1e698b66bb178388
Kenneth <94906413+kenedy69@users.noreply.github.com>
2022-12-01 13:45:05 +0100

    typo controllers

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const notesController = require('../controllers/notesController')
+const notesController = require('../controllers/notesControllers')
 
 router.route('/')
     .get(notesController.getAllNotes)
